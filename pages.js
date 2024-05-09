7e316aa627e5e6ed850f4f35f93e3a19be9ffe7f
Richie Joseph <Rjoseph5196@conestogac.on.ca>
2024-05-09 14:22:24 -0400

    made changes to site

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/Richiedayoungin/home.git";
+const repoURL = "https://github.com/Richiedayoungin/home";
 
 ghpages.publish(
   pathname,
